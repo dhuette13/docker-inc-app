b162b2abbcd4e9123af926417d353a881101c12c
Daniel Huette <dhuette13@yahoo.com>
2016-05-18 16:14:42 -0500

    changed text type for react script

--- web/app.py
+++ web/app.py
@@ -8,7 +8,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/api')
 def hello():
     redis.incr('count')
-    return json.dumps({'count':redis.get('count')})
+    return redis.get('count')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
